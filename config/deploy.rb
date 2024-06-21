8dd189a08a91b358b43bc2facac559ded7aca36d
root <root@ip-172-31-35-96.ec2.internal>
2024-06-21 13:15:05 +0000

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,37 @@
 # config/deploy.rb
+
+# Nombre de la aplicación
 set :application, "sample"
-set :repository,  "path/to/your/repository"
 
-set :scm, :git
-set :branch, "main" # Cambia 'main' por la rama que desees desplegar
+# Configuración del repositorio (none porque no estás usando un repositorio)
+set :repository,  "."
+
+# Configuración del sistema de control de versiones
+set :scm, :none
+set :deploy_via, :copy
+set :copy_strategy, :export
+
+# Directorio de despliegue en el servidor
 set :deploy_to, "/var/www/#{application}"
-set :user, "deploy" # El usuario SSH para conectarse a la instancia EC2
 
-role :web, "3.90.244.138"                          # Tu servidor HTTP
-role :app, "3.90.244.138"                          # Este puede ser el mismo que tu servidor HTTP
-role :db,  "3.90.244.138", :primary => true        # Tu servidor de base de datos, si aplica
+# Usuario para el despliegue
+set :user, "deploy"
 
-# Configuraciones adicionales
+# Roles de los servidores
+role :web, "2.4.6.8"
+role :app, "2.4.6.8"
+role :db,  "2.4.6.8", primary: true
+
+# Opciones SSH
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 set :use_sudo, false
 
+# Tareas de despliegue
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
+  task :restart, roles: :app, except: { no_release: true } do
     run "#{try_sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
   end
 end
