965ee0cbd266f58f2b91786f07274d0cf8fbc9c2
MigueMeza <miguelangelmezavazquez24@hotmail.com>
2024-07-02 22:31:03 +0000

    database-migration

---
+++ app2/config/application.rb
@@ -0,0 +1,20 @@
+#config/application.rb
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+if defined?(Bundler)
+  # Si precompilas assets antes de desplegar a producción, usa esta línea
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # Si quieres compilar assets perezosamente en producción, usa esta línea
+  # Bundler.require(:default, :assets, Rails.env)
+end
+
+module MyApp
+  class Application < Rails::Application
+    # Configuración de la aplicación
+    # Las configuraciones para el entorno/development/test deben estar en los
+    # archivos correspondientes en config/environments
+    # La configuración de la aplicación va aquí
+  end
+end
